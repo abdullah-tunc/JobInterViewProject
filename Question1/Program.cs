3c25f7acc24eccb93e11e139a0287df80d0297af
Abdullah Tunç <abdullahtunc.t@gmail.com>
2023-03-08 11:15:04 +0300

    Add project files.

---
+++ Question1/Program.cs
@@ -0,0 +1,56 @@
+﻿const string validCharacters = "ACDEFGHKLMNPRTXYZ234579";
+
+
+start: Console.WriteLine("Üretilecek kod sayısını giriniz:");
+string input = Console.ReadLine();
+
+int number;
+bool isNumber = int.TryParse(input, out number);
+
+if (isNumber)
+{
+    var codes = GenerateCodes(number);
+    Console.WriteLine($"{number} Adet benzersiz kod üretildi");
+
+
+    foreach (var code in codes)
+        Console.WriteLine(code);
+}
+else
+{
+    Console.WriteLine("Girilen değer bir sayı değildir.");
+    goto start;
+}
+
+
+
+Console.WriteLine("Kontrol edilecek kodu giriniz:");
+string codeInput = Console.ReadLine();
+
+var isValid = CheckCode(codeInput);
+if (isValid)
+    Console.WriteLine("Girilen kod geçerlidir");
+else
+    Console.WriteLine("Girilen kod geçersizdir");
+
+Console.ReadLine();
+static List<string> GenerateCodes(int count)
+{
+    var codes = new List<string>();
+    var random = new Random();
+
+    while (codes.Count < count)
+    {
+        var code = new string(Enumerable.Range(0, 8).Select(_ => validCharacters[random.Next(validCharacters.Length)]).ToArray());
+
+        if (!codes.Contains(code))
+            codes.Add(code);
+    }
+
+    return codes;
+}
+
+static bool CheckCode(string code)
+{
+    return (code.Length == 8) && code.All(c => validCharacters.Contains(c));
+}
---
+++ Question2/Program.cs
@@ -0,0 +1,65 @@
+﻿using Newtonsoft.Json.Linq;
+
+internal class Program
+{
+    static void Main()
+    {
+        string jsonFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "..", "..", "response.json");
+ 
+        string jsonText = File.ReadAllText(jsonFilePath);
+        JArray jsonArray = JArray.Parse(jsonText);
+        int i = 0;
+
+        int prevXCoord = 0;
+        int prevYCoord = 0;
+
+        foreach (var annotation in jsonArray)
+        {
+            if (i != 0)
+            {
+                string description = (string)annotation["description"];
+                var vertices = annotation["boundingPoly"]["vertices"];
+
+                int minX = int.MinValue , minY = int.MinValue;
+                int maxX = int.MaxValue, maxY = int.MaxValue;
+
+                foreach (var vertex in vertices)
+                {
+                    int x = (int)vertex["x"];
+                    int y = (int)vertex["y"];
+
+                    
+                    if (x < minX) minX = x;
+                    if (y < minY) minY = y;
+                    if (x > maxX) maxX = x;
+                    if (y > maxY) maxY = y;
+                }
+
+              
+                int xCoord = (minX + maxX) / 2;
+                int yCoord = (minY + maxY) / 2;
+
+                int newXCoord = xCoord;
+                int newYCoord = yCoord + description.Length + 2;  
+                
+                if (newYCoord == prevYCoord)  
+                {
+                  
+                    Console.SetCursorPosition(prevXCoord, newYCoord);
+                    Console.Write($"{i}| {description}  "); 
+                    prevXCoord += description.Length + 4;  
+                }
+                else
+                {
+                     
+                    Console.SetCursorPosition(newYCoord, newXCoord);
+                    Console.WriteLine($"{i}| {description}");
+                    prevXCoord = newXCoord;
+                    prevYCoord = newYCoord;
+                }
+            }
+
+            i++;
+        }
+    }
+}
